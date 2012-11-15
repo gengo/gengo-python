6194769ce04d9e0322cc57d159f4cb51890d80a2
Shawn Smith <shawnpsmith@gmail.com>
2012-11-15 15:43:03 +0900

    update license for pypi

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD 3-Clause License',
+        'License :: OSI Approved :: BSD License',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet'
     ]
