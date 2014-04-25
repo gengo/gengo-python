ed103e146a109477fb0bb8d70c6da837dcacce0b
Naruse Motoki <motoki@naru.se>
2014-04-25 21:56:59 +0900

    Pyflakes is quite

--- gengo/gengo.py
+++ gengo/gengo.py
@@ -64,19 +64,16 @@ from mockdb import api_urls, apihash
 try:
     # Python 2.6 and up
     import json
-    json  # silence pyflakes
 except ImportError:
     try:
         # Python 2.6 and below (2.4/2.5, 2.3 is not guranteed to work with
         # this library to begin with)
         import simplejson as json
-        json  # silence pyflakes
     except ImportError:
         try:
             # This case gets rarer by the day, but if we need to, we can
             # pull it from Django provided it's there.
             from django.utils import simplejson as json
-            json  # silence pyflakes
         except:
             raise Exception("gengo requires the simplejson library (or " +
                             "Python 2.6+) to work. " +
